37d929df144e9889f45c56db73f8f816ce8336ff
THIENVUAAPP <quocthiencr90@gmail.com>
2026-08-13 17:09:45 +0700

    fix(build): remove puppeteer import from vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { spawn } from 'child_process';
-import puppeteer from 'puppeteer';
 
 export default defineConfig({
   server: {
